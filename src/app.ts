302f3ecb6f774beb3f3ac12a9dc4f8022ec794aa
mike <jfkz@users.noreply.github.com>
2025-02-09 17:11:28 +0000

    fix: change test addres

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,11 @@ async function main() {
     "transactions": [
       // get all transactions coming from and going to our address.
       {
-        to: ['0x52864FfB5Efc4ea7AA15D57bFd55547DEE7F8aC4']
-        // contract_address: ["0xd5b9C4e1f604CB547E5f080A131Be01858D9B399"],
+        to: ['0xac0735f7f9c7b6b4a8ad837c43952f3d44143776']
       },
+      {
+        from: ['0xac0735f7f9c7b6b4a8ad837c43952f3d44143776']
+      }
     ],
     "fieldSelection": {
       "transaction": [
@@ -33,7 +35,7 @@ async function main() {
   // This will parallelize internal requests so we don't have to worry about pipelining/parallelizing make request -> handle response -> handle data loop
   const res = await client.get(query);
 
-  const receiver = await client.stream(query, { reverse: true });
+  const receiver = await client.stream(query, { reverse: false });
 
   console.log(res);
   while (true) {
